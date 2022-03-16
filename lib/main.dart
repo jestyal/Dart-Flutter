1d69438a5e798088e43a1422524181d7832f2099
jestyal <natali.doroshenko14gmail.com>
2022-03-16 22:05:38 +0500

    Added lesson 4 (dart)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+
 import 'package:flutter_app/screens/week2.dart';
 import 'package:flutter_app/screens/week3.dart';
-import 'package:flutter_app/screens/week4_flutter.dart';
+// import 'package:flutter_app/screens/week4_flutter.dart';
+import 'package:flutter_app/screens/week4_dart.dart';
 
 // void main() {
 //   runApp(const MyApp());
@@ -23,7 +25,8 @@ class MyApp extends StatelessWidget {
       ),
       // home: const CongratulationWiget(), //week_2
       // home: const BalloonsWidget(), //week_3
-      home: const TextSizeWidget(), //week_4_flutter
+      // home: const TextSizeWidget(), //week_4_flutter
+      home: const ContactsPage(), //week_4_dart
     );
   }
 }
---
+++ lib/screens/week4_dart.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ContactsPage extends StatelessWidget {
+  const ContactsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Contacts"),
+            centerTitle: true,
+          ),
+          body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: Colors.white,
+            // alignment: Alignment.center,
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    for (int i = 0; i < 5; i++) const ContactItem(),
+                  ],
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
+
+class ContactItem extends StatelessWidget {
+  const ContactItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Container(
+            width: 80,
+            height: 80,
+            margin: const EdgeInsets.only(right: 20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: Colors.grey,
+                width: 1,
+              ),
+              borderRadius: BorderRadius.circular(50.0),
+              image: const DecorationImage(
+                image: AssetImage('assets/images/avatars/ava1.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Column(
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Имя Фамилия",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text("Teл: +7 900 000 00 00"),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text("14.01.1993"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
